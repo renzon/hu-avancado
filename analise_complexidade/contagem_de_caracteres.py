9d35ea6ebf09a0e87051503b6aa2d421250bf098
renzon <renzon@gmail.com>
2018-11-01 15:52:42 -0300

    Solução contagem de caracteres quadrática

--- analise_complexidade/contagem_de_caracteres.py
+++ analise_complexidade/contagem_de_caracteres.py
@@ -1,10 +1,13 @@
 """
 >>> dict(contagem('banana').items())
-{'b':1,'a': 3, 'n': 2 }
+{'b': 1, 'a': 3, 'n': 2}
 >>> dict(contagem('maça').items())
-{'m':1,'a': 2, 'ç': 1 }
+{'m': 1, 'a': 2, 'ç': 1}
 """
 
 
-def contagem(letras):
-    pass
+def contagem(caracteres: str):
+    resultado = {}
+    for c in caracteres:
+        resultado[c] = caracteres.count(c)
+    return resultado
